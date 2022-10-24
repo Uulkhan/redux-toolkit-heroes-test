5f21f66ba229a71295387915d74626e4b9f67da0
UULKHAN M <uulkhan.mairambek@gmail.com>
2022-10-24 19:12:04 +0600

    Initialization

---
+++ src/components/heroesList/heroesSlice.js
@@ -0,0 +1,64 @@
+import {createSlice,createSelector,createAsyncThunk, createEntityAdapter} from '@reduxjs/toolkit';
+import {useHttp} from '../../hooks/http.hook';
+
+const heroesAdapter = createEntityAdapter();
+
+const initialState = heroesAdapter.getInitialState({
+  heroesLoadingStatus: "idle",
+})
+
+export const fetchHeroes = createAsyncThunk(
+  'heroes/fetchHeroes', 
+  async () => {
+    const{request} = useHttp();
+    return await request("http://localhost:3001/heroes")
+  }
+)
+const heroesSlice = createSlice({
+  name: "heroes",
+  initialState,
+  reducers: {
+    heroCreated: (state, action) => {
+      heroesAdapter.addOne(state, action.payload);
+    },
+    heroDeleted: (state, action) => {
+      heroesAdapter.removeOne(state, action.payload);
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+    .addCase(fetchHeroes.pending,state => {state.heroesLoadingStatus = "loading"})
+    .addCase(fetchHeroes.fulfilled,(state, action) => {
+      state.heroesLoadingStatus = "idle";
+      // state.heroes = action.payload;
+      heroesAdapter.setAll(state, action.payload);
+      // 
+    })
+    .addCase(fetchHeroes.rejected,state => {state.heroesLoadingStatus = "error"})
+    .addDefaultCase(()=> {})
+  }
+});
+const {selectAll} = heroesAdapter.getSelectors(state => state.heroes);
+
+export const filteresHeroesSelector = createSelector(
+  //memoization useSelector avoiding rerendered
+  (state) => state.filters.activeFilter,
+  selectAll,
+  (filter, heroes) => {
+    if (filter === "all") {
+      return heroes;
+    } else {
+      return heroes.filter((item) => item.element === filter);
+    }
+  }
+);
+const {actions, reducer} = heroesSlice;
+export default reducer;
+
+export const {
+heroesFetching,
+heroesFetched,
+heroesFetchingError,
+heroCreated,
+heroDeleted
+} = actions;
